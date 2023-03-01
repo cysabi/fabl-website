e8ce3da04c5bdd35b0096bf5cd3fb5f7028a2d48
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2023-03-01 15:43:56 -0500

    Update qualifier section

---
+++ pages/data.js
@@ -0,0 +1,10 @@
+export default {
+  "Qualifier 1": new Date(Date.UTC(2023, 2, 11, 20)),
+  "Qualifier 2": new Date(Date.UTC(2023, 2, 18, 20)),
+  "Qualifier 3": new Date(Date.UTC(2023, 2, 25, 20)),
+  "Qualifier 4": new Date(Date.UTC(2023, 3, 1, 20)),
+  "Qualifier 5": new Date(Date.UTC(2023, 3, 8, 20)),
+  "Qualifier 6": new Date(Date.UTC(2023, 3, 15, 20)),
+  // "FABL-ed Finals": new Date(Date.UTC(2023, 3, 29, 20)),
+  // "Grand Finals": new Date(Date.UTC(2023, 3, 30, 20)),
+};
--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import Layout from "./Layout";
 import { motion } from "framer-motion";
+import {
+  addDays,
+  endOfDay,
+  format,
+  isFuture,
+  isPast,
+  isToday,
+  isWithinInterval,
+  startOfDay,
+} from "date-fns";
+import data from "./data";
 
 export { Page };
 
@@ -16,13 +27,15 @@ function Page() {
       delay,
     },
   });
+  console.log(Object.entries(data));
+
   return (
     <Layout>
       <motion.div
         className="p-8 w-full flex flex-col items-center justify-center bg-fixed min-h-[calc(100vh-8rem)]"
         style={{
-          backgroundSize: "auto 75%",
           backgroundPosition: "center",
+          backgroundSize: "auto 75%",
           backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='40' viewBox='0 0 50 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%232e225c' fill-opacity='0.33'%3E%3Cpath d='M40 10L36.67 0h-2.11l3.33 10H20l-2.28 6.84L12.11 0H10l6.67 20H10l-2.28 6.84L2.11 10 5.44 0h-2.1L0 10l6.67 20-3.34 10h2.11l2.28-6.84L10 40h20l2.28-6.84L34.56 40h2.1l-3.33-10H40l2.28-6.84L47.89 40H50l-6.67-20L50 0h-2.1l-5.62 16.84L40 10zm1.23 10l-2.28-6.84L34 28h4.56l2.67-8zm-10.67 8l-2-6h-9.12l2 6h9.12zm-12.84-4.84L12.77 38h15.79l2.67-8H20l-2.28-6.84zM18.77 20H30l2.28 6.84L37.23 12H21.44l-2.67 8zm-7.33 2H16l-4.95 14.84L8.77 30l2.67-8z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
         }}
       >
@@ -100,20 +113,7 @@ function Page() {
             stroke="none"
           />
         </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="w-12 h-12 text-white absolute inset-0 mx-auto my-auto"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
-          />
-        </svg>
+        <DownArrow />
       </div>
       <div className="text-center font-medium p-6 sm:p-12 flex flex-col items-center justify-evenly min-h-[calc(100vh-8rem)] text-xl sm:text-2xl gap-6 sm:gap-12">
         <div>
@@ -174,55 +174,234 @@ function Page() {
       </div>
       <div className="text-center font-medium p-6 sm:p-12 flex flex-col items-center justify-evenly min-h-[calc(100vh-8rem)] text-xl sm:text-2xl">
         <div>
-          <div className="font-bold text-3xl sm:text-4xl text-center">
+          <h2 className="font-bold text-3xl sm:text-4xl text-center" id="play">
             Ready to play?
-          </div>
-          <p className="text-fabl-indigo-100 ">
+          </h2>
+          <p className="text-fabl-indigo-100">
             Check the status of upcoming qualifiers, and see whether you can
             sign up, don't be late!
           </p>
         </div>
-        <div className="w-full mx-auto max-w-lg p-4 bg-green-400/20 rounded-lg flex flex-col gap-3">
-          <div className="flex items-center gap-3 text-lg uppercase font-semibold tracking-wider text-green-400">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-              />
-            </svg>
-            <p>Qualifer 2</p>
-          </div>
-          <div className="flex items-center gap-3">Open for registration!</div>
+        <div className="flex flex-col gap-2 max-w-lg w-full">
+          <QualifierStatus />
         </div>
-        <div className="w-full mx-auto max-w-lg flex flex-col gap-4">
-          <Qualifer name="Qualifer 1" status="concluded" />
-          <Qualifer name="Qualifer 2" status="concluded" />
-          <Qualifer name="Qualifer 3" status="open" />
-          <Qualifer name="Qualifer 4" status="future" />
-          <Qualifer name="Qualifer 5" status="future" />
-          <Qualifer name="Qualifer 6" status="future" />
+        <div className="flex flex-col gap-2 max-w-lg w-full">
+          <h3 className="text-xl font-semibold text-fabl-indigo-200 uppercase tracking-wider">
+            All Qualifiers
+          </h3>
+          <div className="w-full flex flex-col">
+            {Object.entries(data).map(([key, value]) => (
+              <Qualifer key={key} name={key} date={value} />
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
   );
 }
 
-const Qualifer = ({ name, status }) => (
-  <a
-    className={`rounded-md flex flex-1 w-full items-center justify-between ${
-      status === "open" &&
-      "font-semibold text-green-400 hover:bg-fabl-indigo-700"
-    } ${status === "future" && "text-fabl-indigo-200"}`}
-  >
-    <div>{name}</div>
-    <div>{status}</div>
-  </a>
-);
+const DownArrow = () => {
+  const ref = useRef();
+
+  return (
+    <motion.button
+      className="w-12 h-12 text-white absolute inset-0 mx-auto my-auto hover:cursor-pointer"
+      onClick={() => {
+        ref.current.scrollIntoView({ behavior: "smooth" });
+      }}
+      whileHover={{
+        scale: 1.15,
+      }}
+      transition={{
+        type: "spring",
+        duration: 0.8,
+      }}
+    >
+      <motion.svg
+        ref={ref}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        animate={{ y: [4, -4] }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut",
+          duration: 2,
+        }}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
+        />
+      </motion.svg>
+    </motion.button>
+  );
+};
+
+const QualifierStatus = () => {
+  const current = Object.entries(data).reduce((p, c) => {
+    if (isToday(p[1]) || isFuture(p[1])) {
+      return p;
+    }
+    return c;
+  });
+  const status = getStatus(current[1]);
+
+  return (
+    <>
+      <h3 className="text-xl font-semibold text-fabl-indigo-200 uppercase tracking-wider">
+        {status === "now" ? "Current" : "Upcoming"} Qualifier
+      </h3>
+      <a
+        href={status === "now" ? "https://otd.ink/signup" : undefined}
+        className={`w-full p-4 rounded-lg flex flex-col group gap-3 ${
+          status === "now" &&
+          "bg-green-400/20 hover:bg-green-400/25 text-green-400"
+        } ${status === "future" && "bg-blue-400/20 text-blue-400"} ${
+          status === "past" && "bg-fabl-indigo-700"
+        }`}
+      >
+        <div className="flex items-center justify-between gap-3 text-xl font-semibold flex-wrap">
+          {status !== "past" && (
+            <div className="flex items-center gap-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 shrink-0"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.348 14.651a3.75 3.75 0 010-5.303m5.304 0a3.75 3.75 0 010 5.303m-7.425 2.122a6.75 6.75 0 010-9.546m9.546 0a6.75 6.75 0 010 9.546M5.106 18.894c-3.808-3.808-3.808-9.98 0-13.789m13.788 0c3.808 3.808 3.808 9.981 0 13.79M12 12h.008v.007H12V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                />
+              </svg>
+              <p>{current[0]}</p>
+            </div>
+          )}
+          {status === "now" ? (
+            <div className="flex items-center gap-3">
+              <p>Registration is open!</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6 group-hover:translate-x-1 transition-transform"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          ) : (
+            <>
+              {status === "future" && (
+                <p>Registration opens {format(current[1], "MMMM do")}</p>
+              )}
+              {status === "past" && (
+                <p className="text-center w-full">
+                  All events have concluded, thanks for playing!
+                </p>
+              )}
+            </>
+          )}
+        </div>
+      </a>
+    </>
+  );
+};
+
+const Qualifer = ({ name, date }) => {
+  const status = getStatus(date);
+
+  return (
+    <div
+      className={`rounded-md text-xl px-3 py-2 flex flex-1 w-full items-center justify-between ${
+        status === "now" && "font-semibold text-green-400"
+      } ${status === "past" && "text-fabl-indigo-400"} ${
+        status === "future" && "text-fabl-indigo-100"
+      }`}
+    >
+      <div className="flex items-center gap-3">
+        {status === "past" && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-fabl-indigo-500"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        )}
+        {status === "now" && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+            />
+          </svg>
+        )}
+        {status === "future" && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-fabl-indigo-300"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        )}
+        <span>{name}</span>
+      </div>
+      <div>{format(date, "MMM do")}</div>
+    </div>
+  );
+};
+
+const getStatus = (date) => {
+  if (
+    isWithinInterval(new Date(), {
+      start: date,
+      end: addDays(date, 1),
+    })
+  ) {
+    return "now";
+  } else if (isFuture(date)) {
+    return "future";
+  } else {
+    return "past";
+  }
+};
