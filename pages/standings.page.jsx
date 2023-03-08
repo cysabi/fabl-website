ccaa2c301b1f735a1dc9ae4d57255efc298f2d69
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2023-03-08 16:07:10 -0500

    Query database for leaderboard

--- pages/standings.page.jsx
+++ pages/standings.page.jsx
@@ -1,52 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./Layout";
 
 export { Page };
 
 function Page() {
-  const data = false;
+  const data = useData();
+  console.log(data);
 
   return (
     <Layout>
       <div className="my-12 gap-12 flex w-full px-4 flex-col">
-        <div className="text-3xl font-bold text-center">Current Standings</div>
-        {!data ? (
-          <div className="w-full flex flex-col gap-3">
-            <Row
-              username={
-                <div className="text-center w-full overflow-auto whitespace-normal">
-                  Qualifiers have not begun yet, so standings are not avaliable.
-                  Check back soon!
-                </div>
-              }
-            />
-            {Array.from({ length: 9 }).map((v, i) => (
-              <div style={{ opacity: `calc(100% - ${(i + 1) * 10}%)` }}>
-                <Row username={`\u200B`} />
-              </div>
-            ))}
-          </div>
-        ) : (
-          <Table />
-        )}
+        <div className="text-2xl sm:text-3xl font-bold text-center">
+          Current Standings
+        </div>
+        <Table data={data} />
       </div>
     </Layout>
   );
 }
 
-const Table = () => (
-  <div className="w-full flex flex-col gap-3">
-    {Array.from({ length: 20 }).map((v, i) => (
-      <Row placement={i + 1} username="Maria Anders" points={8274 - i * 435} />
-    ))}
-  </div>
-);
+const Table = ({ data }) => {
+  if (data === undefined) {
+    return (
+      <div className="w-full flex flex-col gap-3 animate-pulse">
+        {Array.from({ length: 9 }).map((v, i) => (
+          <div key={i} style={{ opacity: `calc(100% - ${(i + 1) * 10}%)` }}>
+            <Row name={`\u200B`} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 
-const Row = ({ placement, username, points }) => (
-  <div className="flex items-center text-2xl max-w-xl mx-auto w-full">
+  if (data.length === 0) {
+    return (
+      <div className="w-full flex flex-col gap-3">
+        <Row
+          name={
+            <div className="text-center w-full overflow-auto whitespace-normal">
+              Qualifiers have not begun yet, so standings are not avaliable.
+              Check back soon!
+            </div>
+          }
+        />
+        {Array.from({ length: 9 }).map((v, i) => (
+          <div key={i} style={{ opacity: `calc(100% - ${(i + 1) * 10}%)` }}>
+            <Row name={`\u200B`} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+  return (
+    <div className="w-full flex flex-col gap-3">
+      {data.map((v, i) => (
+        <Row
+          placement={i + 1}
+          key={i}
+          name={
+            <div className="flex items-center gap-3">
+              <span>{v.splashtag}</span>
+              <div className="flex items-center gap-2">
+                {v.weapons.map((weapon) => (
+                  <img
+                    key={weapon.id}
+                    src={`https://raw.githubusercontent.com/Sendouc/sendou.ink/rewrite/public/static-assets/img/main-weapons-outlined/${weapon.id}.png`}
+                    className="h-7 w-7 sm:h-8 sm:w-8"
+                  />
+                ))}
+              </div>
+            </div>
+          }
+          points={v.points}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Row = ({ placement, name, points }) => (
+  <div className="flex items-center text-xl sm:text-2xl max-w-3xl mx-auto w-full">
     {placement && (
       <div
-        className={`w-20 shrink-0 text-center font-black text-fabl-indigo-light ${
+        className={`shrink-0 pr-4 sm:pr-0 sm:w-16 sm:text-center font-black text-fabl-indigo-light ${
           placement <= 1
             ? "!text-fabl-gold"
             : placement <= 3
@@ -62,7 +98,7 @@ const Row = ({ placement, username, points }) => (
         placement % 2 === 0 ? "bg-fabl-indigo-700" : "bg-fabl-indigo-700"
       }`}
     >
-      <div className="flex-1 truncate">{username}</div>
+      <div className="flex-1 truncate">{name}</div>
       {points && (
         <div className="font-mono shrink-0">
           <span className="font-bold">{points}</span>{" "}
@@ -70,6 +106,20 @@ const Row = ({ placement, username, points }) => (
         </div>
       )}
     </div>
-    {placement && <div className="sm:w-20"></div>}
+    {placement && <div className="sm:w-16"></div>}
   </div>
 );
+
+const useData = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(
+      "https://us-central1-off-the-dial-26e93.cloudfunctions.net/fabl-standings"
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return data;
+};
