3980b4064ec72ed65808978b121300b840ee6463
DJam98 <djam98@otd.ink>
2023-03-21 00:13:06 +1100

    Q3

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export default {
   "Qualifier 1": { date: new Date(Date.UTC(2023, 2, 11, 20)), open: false },
-  "Qualifier 2": { date: new Date(Date.UTC(2023, 2, 18, 20)), open: true },
-  "Qualifier 3": { date: new Date(Date.UTC(2023, 2, 25, 20)), open: false },
+  "Qualifier 2": { date: new Date(Date.UTC(2023, 2, 18, 20)), open: false },
+  "Qualifier 3": { date: new Date(Date.UTC(2023, 2, 25, 20)), open: true },
   "Qualifier 4": { date: new Date(Date.UTC(2023, 3, 1, 20)), open: false },
   "Qualifier 5": { date: new Date(Date.UTC(2023, 3, 8, 20)), open: false },
   "Qualifier 6": { date: new Date(Date.UTC(2023, 3, 15, 20)), open: false },
