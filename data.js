383d3981204faa13167c4a4eb9e6bbab56a6763d
DJam98 <djam98@otd.ink>
2023-03-26 21:22:20 +1100

    q4

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 export default {
   "Qualifier 1": { date: new Date(Date.UTC(2023, 2, 11, 20)), open: false },
   "Qualifier 2": { date: new Date(Date.UTC(2023, 2, 18, 20)), open: false },
-  "Qualifier 3": { date: new Date(Date.UTC(2023, 2, 25, 20)), open: true },
-  "Qualifier 4": { date: new Date(Date.UTC(2023, 3, 1, 20)), open: false },
+  "Qualifier 3": { date: new Date(Date.UTC(2023, 2, 25, 20)), open: false },
+  "Qualifier 4": { date: new Date(Date.UTC(2023, 3, 1, 20)), open: true },
   "Qualifier 5": { date: new Date(Date.UTC(2023, 3, 8, 20)), open: false },
   "Qualifier 6": { date: new Date(Date.UTC(2023, 3, 15, 20)), open: false },
   // "FABL-ed Finals": new Date(Date.UTC(2023, 3, 29, 20)),
